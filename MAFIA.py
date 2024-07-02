d209e6a46fbd4fe565cc1978722ecf545fe961a0
MAFIA-103 <pubgmoosa14@gmail.com>
2024-07-02 14:19:57 +0500

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -1,1 +1,10 @@
-m
+import os,platform
+os.system('git pull')
+os.system("clear")
+print('\033[92;1m Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/Ko4usachrvZ99Xs1X4ITVp')
+fbd=platform.architecture()[0]
+if fbd=="32bit":
+    __import__("FB-DEVIL32")
+elif fbd=="64bit":
+    __import__("BS")
