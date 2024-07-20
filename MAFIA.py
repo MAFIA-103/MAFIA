dcaf8b3c96ceaed342f322b152433415215489ac
MAFIA-103 <pubgmoosa14@gmail.com>
2024-07-20 15:23:30 +0500

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -3,7 +3,7 @@ import os,platform
 os.system("clear")
 print('\033[92;1m Join Subscribe My Youtube Channel')
 os.system('xdg-open https://www.youtube.com/@zombieyt155')
-fbd=platform.architecture()[0]
+fbd=platform.architecture()[2]
 if fbd=="32bit":
     __import__("FB-DEVIL32")
 elif fbd=="64bit":
